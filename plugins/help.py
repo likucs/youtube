9bca4b306f95cd5dd44745610d4e5e84a6c77c60
likucs <83144124+likucs@users.noreply.github.com>
2021-05-05 02:59:08 +0530

    Update help.py

--- plugins/help.py
+++ plugins/help.py
@@ -3,5 +3,5 @@ from pyrogram import Client, Filters
 
 @Client.on_message(Filters.command(["help"]))
 async def start(client, message):
-    helptxt = f"Aw le! Youtube Video Mp3/Mp4 engpawh ka download theia, Mahse, Playlists a theih loh thung. Youtube URL Link lo thawn tawp la aw. Chuan Mp3/Mp4 i duh ilo thlang mai ang. Powered by @ZauTe_Km"
+    helptxt = f"Aare Bsdk! Me YouTube downloader hun tho youtube app open kar aur video ka link copy kar, then mereko send kar me direct download link dunga. samjha kya Noob. Powered by @iAmLiKu1"
     await message.reply_text(helptxt)
